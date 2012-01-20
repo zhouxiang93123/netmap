402b9b633ba0cfe8aa4404028ced43b81b2e92e5
Luigi Rizzo <rizzo@iet.unipi.it>
2012-01-20 14:45:09 +0000

    prepare to work with cxgbe

--- sys/dev/netmap/cxgbe_netmap.h
+++ sys/dev/netmap/cxgbe_netmap.h
@@ -1,8 +1,29 @@
 /*
- * (C) 2011 Luigi Rizzo, Matteo Landi - Universita` di Pisa
+ * Copyright (C) 2012 Luigi Rizzo. All rights reserved.
  *
- * BSD Copyright
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
  * $FreeBSD$
  * $Id: cxgbe_netmap.h 9520 2011-10-24 14:11:01Z svn_landi $
  *
@@ -43,7 +64,7 @@ cxgbe_netmap_attach(struct port_info *pi)
 	 * we allocate the buffers on the first register. So we must
 	 * disallow a SIOCSETMTU when if_capenable & IFCAP_NETMAP is set.
 	 */
-	na.buff_size = MCLBYTES;
+	na.buff_size = NETMAP_BUF_SIZE;
 	netmap_attach(&na, pi->ntxq);
 }	
 
