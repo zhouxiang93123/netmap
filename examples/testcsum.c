0c0ae83281d2e7016e95f5229e923534644916a3
Luigi Rizzo <rizzo@iet.unipi.it>
2012-02-28 09:32:18 +0000

    fix overflow on loop unrolling

--- examples/testcsum.c
+++ examples/testcsum.c
@@ -12,14 +12,14 @@ uint32_t
 sum16(const unsigned char *addr, int count)
 {
         uint32_t sum = 0;
-        int i;
+	uint16_t *d = (uint16_t *)addr;
                           
-        for (i = 0; count >= 2; i+= 2, count -= 2)
-                sum += * (uint16_t *) (addr + i);
+        for (;count >= 2; count -= 2)
+                sum += *d++;
          
         /* Add left-over byte, if any */
         if (count & 1)
-                sum += *(addr + i);
+                sum += *(uint8_t *)d;
 	sum = (sum & 0xffff) + (sum >> 16);
 	sum = (sum & 0xffff) + (sum >> 16);
         return sum;
@@ -55,11 +55,11 @@ sum32u(const unsigned char *addr, int count)
 	//const uint8_t *c = addr;
 
 	for (; count >= 32; count -= 32) {
-		sum += p[0] + p[1] + p[2] + p[3] + p[4] + p[5] + p[6] + p[7];
+		sum += (uint64_t)p[0] + p[1] + p[2] + p[3] + p[4] + p[5] + p[6] + p[7];
 		p += 8;
 	}
-	for (; count >= 16; count -= 16) {
-		sum += p[0] + p[1] + p[2] + p[3];
+	for (;1 &&  count >= 16; count -= 16) {
+		sum += (uint64_t)p[0] + p[1] + p[2] + p[3];
 		p += 4;
 	}
 	for (; count >= 4; count -= 4) {
@@ -72,11 +72,10 @@ sum32u(const unsigned char *addr, int count)
 	}
         if (count & 1)
                 sum += *addr;
-//fprintf(stderr, "count %d at %ld sum %lu\n", count, addr - c, sum);
 	sum = (sum & 0xffffffff) + (sum >> 32);
 	sum = (sum & 0xffffffff) + (sum >> 32);
-	sum = (sum & 0xffff) + (sum >> 16);
-	sum = (sum & 0xffff) + (sum >> 16);
+	sum = (sum & 0xffff) + ((sum >> 16) & 0xffff);
+	sum = (sum & 0xffff) + ((sum >> 16) & 0xffff);
 	return sum;
 }
 
@@ -95,7 +94,7 @@ main(int argc, char *argv[])
 		buf[i] = i *i - i + 5;
 	for (n = 0; n < lim; n++) {
 		for (i = 0; i < 1000000; i++) {
-			res = sum32((unsigned char *)buf, len);
+			res = sum32u((unsigned char *)buf, len);
 		}
 	}
 	fprintf(stderr, "csum 32 %u csum16 %d csum32u %u\n",
