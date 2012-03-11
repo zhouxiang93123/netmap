ef09ff65e8c3da0cc06ebdb5e56e7e71d0176ce8
Luigi Rizzo <rizzo@iet.unipi.it>
2012-03-11 21:06:08 +0000

    snapshot of various checksum solutions. interestingly, the assembler version is slow!

--- examples/testcsum.c
+++ examples/testcsum.c
@@ -4,9 +4,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <inttypes.h>
 
 volatile uint16_t res;
+
+#define REDUCE16(_x)	({ uint32_t x = _x;	\
+	x = (x & 0xffff) + (x >> 16);		\
+	x = (x & 0xffff) + (x >> 16);		\
+	x; } )
+
+#define REDUCE32(_x)	({ uint64_t x = _x;	\
+	x = (x & 0xffffffff) + (x >> 32);	\
+	x = (x & 0xffffffff) + (x >> 32);	\
+	x; } )
+
+uint32_t
+dummy(const unsigned char *addr __unused, int count __unused)
+{
+	return 0;
+}
 
 uint32_t
 sum16(const unsigned char *addr, int count)
@@ -20,9 +37,7 @@ sum16(const unsigned char *addr, int count)
         /* Add left-over byte, if any */
         if (count & 1)
                 sum += *(uint8_t *)d;
-	sum = (sum & 0xffff) + (sum >> 16);
-	sum = (sum & 0xffff) + (sum >> 16);
-        return sum;
+        return REDUCE16(sum);
 }
 
 uint32_t
@@ -40,11 +55,8 @@ sum32(const unsigned char *addr, int count)
         /* Add left-over byte, if any */
         if (++i == count)
                 sum += *(addr + i);
-	sum = (sum & 0xffffffff) + (sum >> 32);
-	sum = (sum & 0xffffffff) + (sum >> 32);
-	sum = (sum & 0xffff) + (sum >> 16);
-	sum = (sum & 0xffff) + (sum >> 16);
-        return sum;
+	sum = REDUCE32(sum);
+        return REDUCE16(sum);
 }
 
 uint32_t
@@ -58,6 +70,71 @@ sum32u(const unsigned char *addr, int count)
 		sum += (uint64_t)p[0] + p[1] + p[2] + p[3] + p[4] + p[5] + p[6] + p[7];
 		p += 8;
 	}
+	for (;count >= 16; count -= 16) {
+		sum += (uint64_t)p[0] + p[1] + p[2] + p[3];
+		p += 4;
+	}
+	for (; count >= 4; count -= 4) {
+		sum += *p++;
+	}
+	addr = (unsigned char *)p;
+	if (count > 1) {
+                sum += *(uint16_t *)addr;
+		addr += 2;
+	}
+        if (count & 1)
+                sum += *addr;
+	sum = REDUCE32(sum);
+	return REDUCE16(sum);
+}
+
+uint32_t
+sum32a(const unsigned char *addr, int count)
+{
+        uint32_t sum32 = 0;
+        uint64_t sum;
+	const uint32_t *p = (const uint32_t *)addr;
+
+	for (;count >= 64; count -= 64) {
+	    __asm(
+                "add %1, %0\n"
+                "adc %2, %0\n"
+                "adc %3, %0\n"
+                "adc %4, %0\n"
+                "adc %5, %0\n"
+                "adc %6, %0\n"
+                "adc %7, %0\n"
+                "adc %8, %0\n"
+                "adc %9, %0\n"
+                "adc %10, %0\n"
+                "adc %11, %0\n"
+                "adc %12, %0\n"
+                "adc %13, %0\n"
+                "adc %14, %0\n"
+                "adc %15, %0\n"
+                "adc %16, %0\n"
+                "adc $0, %0"
+                : "+r" (sum32)
+                : "g" (p[0]),
+                  "g" (p[1]),
+                  "g" (p[2]),
+                  "g" (p[3]),
+                  "g" (p[4]),
+                  "g" (p[5]),
+                  "g" (p[6]),
+                  "g" (p[8]),
+                  "g" (p[9]),
+                  "g" (p[10]),
+                  "g" (p[11]),
+                  "g" (p[12]),
+                  "g" (p[13]),
+                  "g" (p[14]),
+                  "g" (p[15])
+                : "cc"
+	    );
+	    p += 16;
+	}
+	sum = sum32;
 	for (;1 &&  count >= 16; count -= 16) {
 		sum += (uint64_t)p[0] + p[1] + p[2] + p[3];
 		p += 4;
@@ -74,30 +151,59 @@ sum32u(const unsigned char *addr, int count)
                 sum += *addr;
 	sum = (sum & 0xffffffff) + (sum >> 32);
 	sum = (sum & 0xffffffff) + (sum >> 32);
-	sum = (sum & 0xffff) + ((sum >> 16) & 0xffff);
-	sum = (sum & 0xffff) + ((sum >> 16) & 0xffff);
-	return sum;
+        sum = (sum & 0xffff) + (sum >> 16);
+        sum = (sum & 0xffff) + (sum >> 16);
+        return sum;
 }
+
+
+struct ftab {
+	char *name;
+	uint32_t (*fn)(const unsigned char *, int);
+};
+
+struct ftab f[] = {
+	{ "dummy", dummy },
+	{ "sum16", sum16 },
+	{ "sum32", sum32 },
+	{ "sum32u", sum32u },
+	{ "sum32a", sum32a },
+	{ NULL, NULL }
+};
 
 int
 main(int argc, char *argv[])
 {
+	int i, n;
 	int lim = argc > 1 ? atoi(argv[1]) : 100;
 	int len = argc > 2 ? atoi(argv[2]) : 1024;
-	int i, n;
+	char *fn = argc > 3 ? argv[3] : "sum16";
 	char buf[2048];
-	// uint32_t (*fn)(const unsigned char *addr, int count);
+	uint32_t (*fnp)(const unsigned char *, int) = NULL;
 
+	for (i = 0; f[i].name; i++) {
+		if (!strcmp(f[i].name, fn)) {
+			fnp = f[i].fn;
+			break;
+		}
+	}
+	if (fnp == NULL) {
+		fnp = sum16;
+		fn = "sum16-default";
+	}
 	if (len > (int)sizeof(buf))
 		len = sizeof(buf);
 	for (i = 0; i < len; i++)
 		buf[i] = i *i - i + 5;
+	fprintf(stderr, "function %s len %d count %dM\n",
+		fn, len, lim);
 	for (n = 0; n < lim; n++) {
 		for (i = 0; i < 1000000; i++) {
-			res = sum32u((unsigned char *)buf, len);
+			res = fnp((unsigned char *)buf, len);
 		}
 	}
-	fprintf(stderr, "csum 32 %u csum16 %d csum32u %u\n",
+	fprintf(stderr, "%s %u sum16 %u sum32 %d sum32u %u\n",
+		fn, res,
 		sum16((unsigned char *)buf, len),
 		sum32((unsigned char *)buf, len),
 		sum32u((unsigned char *)buf, len));
