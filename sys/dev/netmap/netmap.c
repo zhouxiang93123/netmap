zhouxiang93123/netmap

commit e9c645d7c8084df8381f2719309dd54995803b09

move the two net_device_ops outside the struct netmap_adapter.

sys/dev/netmap/netmap.c
@@ -2784,13 +2784,21 @@ netmap_attach(struct netmap_adapter *arg, u_int num_queues)
 {
 	struct netmap_adapter *na = NULL;
 	struct ifnet *ifp = arg ? arg->ifp : NULL;
-	size_t len;
+	int na_entries;
 
 	if (arg == NULL || ifp == NULL)
 		goto fail;
-	/* a VALE port uses two endpoints */
-	len = nma_is_vp(arg) ? sizeof(*na) : sizeof(*na) * 2;
-	na = malloc(len, M_DEVBUF, M_NOWAIT | M_ZERO);
+	/*
+	 * Allocate 1 struct netmap_adapter (or 2 for a NIC,
+	 * the second being used for the host-stack port),
+	 * and memory for a couple of blocks to override
+	 * linux callbacks. These are only used on physical
+	 * ports (entry 0).
+	 */
+	na_entries = nma_is_vp(arg) ? 1 : 2;
+	na = malloc(na_entries * sizeof(*na) +
+		2 * sizeof(struct net_device_ops),
+		M_DEVBUF, M_NOWAIT | M_ZERO);
 	if (na == NULL)
 		goto fail;
 	WNA(ifp) = na;
@@ -2806,18 +2814,26 @@ netmap_attach(struct netmap_adapter *arg, u_int num_queues)
 #ifdef linux
 	// XXX move to linux_specific code
 	// linux_netmap_attach(na); // XXX complete
-	if (ifp->netdev_ops) {
-		ND("netdev_ops %p", ifp->netdev_ops);
-		/* prepare a clone of the netdev ops */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 28)
-		na->nm_ndo.ndo_start_xmit = ifp->netdev_ops;
-#else
-		na->nm_ndo = *ifp->netdev_ops;
-#endif
+	if (!ifp->netdev_ops) {
+		D("missing netdev_ops, cannot proceed");
+		free(na, M_DEVBUF);
+		goto fail;
 	}
-	na->nm_ndo.ndo_start_xmit = linux_netmap_start_xmit;
+
+	/* connect the netdev_ops buffers to na[0] */
+	na->nm_ndo_p = (void *)(na + na_entries);
+	na->generic_ndo_p = na->nm_ndo_p + 1;
+
+	/* prepare a clone of the netdev ops */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 28)
+	na->nm_ndo_p->ndo_start_xmit = ifp->netdev_ops;
+#else
+	*na->nm_ndo_p = *ifp->netdev_ops;
+#endif
+	na->nm_ndo_p->ndo_start_xmit = linux_netmap_start_xmit;
 #endif /* linux */
 
+	/* reference the memory allocator in use */
 	na->nm_mem = arg->nm_mem ? arg->nm_mem : &nm_mem;
 	if (!nma_is_vp(arg))
 		netmap_attach_sw(ifp);

sys/dev/netmap/netmap_generic.c
@@ -196,9 +196,9 @@ int generic_netmap_register(struct ifnet *ifp, int enable)
         }
         ifp->if_capenable |= IFCAP_NETMAP;
         na->if_transmit = (void *)ifp->netdev_ops;
-        na->generic_ndo = *(ifp->netdev_ops);  /* Copy */
-        na->generic_ndo.ndo_select_queue = &generic_ndo_select_queue;  /* Replace a field. */
-        ifp->netdev_ops = &na->generic_ndo;  /* Switch the pointers. */
+        *na->generic_ndo_p = *ifp->netdev_ops;  /* Copy */
+        na->generic_ndo_p->ndo_select_queue = &generic_ndo_select_queue;  /* Replace a field. */
+        ifp->netdev_ops = na->generic_ndo_p;  /* Switch the pointers. */
 #ifdef RATE
         if (rate_ctx.refcount == 0) {
             D("setup_timer()");
@@ -600,26 +600,20 @@ generic_find_num_desc(struct ifnet *ifp, unsigned int *tx, unsigned int *rx)
 int
 generic_netmap_attach(struct ifnet *ifp)
 {
-    /* if *na is too large we do not want it on stack */
-    struct netmap_adapter *na;
+    struct netmap_adapter na;
     int retval;
     unsigned int num_tx_desc = 256, num_rx_desc = 256;
 
     generic_find_num_desc(ifp, &num_tx_desc, &num_rx_desc);
     D("Netmap ring descriptors: TX = %d, RX = %d\n", num_tx_desc, num_rx_desc);
 
-    na = malloc(sizeof(*na), M_DEVBUF, M_NOWAIT | M_ZERO);
-    if (na == NULL) {
-	D("no memory on attach, give up");
-	return ENOMEM;
-    }
-    bzero(na, sizeof(*na));
-    na->ifp = ifp;
-    na->num_tx_desc = num_tx_desc;
-    na->num_rx_desc = num_rx_desc;
-    na->nm_register = &generic_netmap_register;
-    na->nm_txsync = &generic_netmap_txsync;
-    na->nm_rxsync = &generic_netmap_rxsync;
+    bzero(&na, sizeof(na));
+    na.ifp = ifp;
+    na.num_tx_desc = num_tx_desc;
+    na.num_rx_desc = num_rx_desc;
+    na.nm_register = &generic_netmap_register;
+    na.nm_txsync = &generic_netmap_txsync;
+    na.nm_rxsync = &generic_netmap_rxsync;
 
     ND("[GNA] num_tx_queues(%d), real_num_tx_queues(%d), len(%lu)", ifp->num_tx_queues,
                                         ifp->real_num_tx_queues, ifp->tx_queue_len);
@@ -627,10 +621,9 @@ generic_netmap_attach(struct ifnet *ifp)
                                                             ifp->real_num_rx_queues);
 #ifdef __FreeBSD__
 #else /* linux */
-    na->num_tx_rings = ifp->real_num_tx_queues;
+    na.num_tx_rings = ifp->real_num_tx_queues;
 #endif /* linux */
 
-    retval = netmap_attach(na, 1); // TODO ifp->real_num_rx_queues);
-    free(na, M_DEVBUF);
+    retval = netmap_attach(&na, 1); // TODO ifp->real_num_rx_queues);
     return retval;
 }

sys/dev/netmap/netmap_kern.h
@@ -45,6 +45,12 @@
 
 #define NM_ATOMIC_T	volatile int
 
+// XXX linux struct, not used in FreeBSD
+struct net_device_ops {
+};
+struct hrtimer {
+};
+
 #elif defined (linux)
 
 #define	NM_LOCK_T	safe_spinlock_t	// see bsd_glue.h
@@ -356,18 +362,16 @@ struct netmap_adapter {
         /* Pointer to a previously used netmap adapter. */
         struct netmap_adapter *prev;
 
-#ifdef linux
-	struct net_device_ops nm_ndo;
+	struct net_device_ops *nm_ndo_p;
 
         /* With generic netmap adapters we need a net_device_ops structure to override the
            ndo_select_queue() driver method. */
-        struct net_device_ops generic_ndo;
+        struct net_device_ops *generic_ndo_p;
 
         /* High resolution timer used to mitigate rx notifications with generic netmap
            adapters. */
         struct hrtimer mit_timer;
         int mit_pending;
-#endif /* linux */
 };
 
 /*

commit dd566383c1dfa185586be8d1d972de3c35a59580

fixed NULL-pointer dereference bug
introduced by commit e9c645d7c8084df8381f2719309dd54995803b09

sys/dev/netmap/netmap.c
@@ -2608,23 +2608,20 @@ netmap_attach(struct netmap_adapter *arg, u_int num_queues)
 	mtx_init(&na->core_lock, "netmap core lock", MTX_NETWORK_LOCK, MTX_DEF);
 
 #ifdef linux
-	if (!ifp->netdev_ops) {
-		D("missing netdev_ops, cannot proceed");
-		free(na, M_DEVBUF);
-		goto fail;
-	}
-
 	/* connect the netdev_ops buffers to na[0] */
 	na->nm_ndo_p = (void *)(na + na_entries);
 	na->generic_ndo_p = na->nm_ndo_p + 1;
 
-	/* prepare a clone of the netdev ops */
+        /* ifp->netdev_ops is non null only for physical ports. */
+	if (ifp->netdev_ops) {
+            /* prepare a clone of the netdev ops */
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 28)
-	na->nm_ndo_p->ndo_start_xmit = ifp->netdev_ops;
+            na->nm_ndo_p->ndo_start_xmit = ifp->netdev_ops;
 #else
-	*na->nm_ndo_p = *ifp->netdev_ops;
+            *na->nm_ndo_p = *ifp->netdev_ops;
 #endif
-	na->nm_ndo_p->ndo_start_xmit = linux_netmap_start_xmit;
+            na->nm_ndo_p->ndo_start_xmit = linux_netmap_start_xmit;
+        }
 #endif /* linux */
 
 	/* reference the memory allocator in use */